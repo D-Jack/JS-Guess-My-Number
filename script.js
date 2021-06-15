35a4bd671b7b1f1d1726200b560d1d9e04fb764f
Siddharth <siddharthauchiha50@gmail.com>
2021-06-15 12:00:03 +0530

    refactoring code for Don't repeat yourself (DRY)

--- script.js
+++ script.js
@@ -4,17 +4,22 @@ let secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 let score = 20;
 let highscore = 0;
+
+const displayMessage = message => {
+  document.querySelector('.message').textContent = message;
+};
+
 document.querySelector('.check').addEventListener('click', function () {
   let guess = Number(document.querySelector('.guess').value);
   console.log(guess);
 
   //when there is no number
   if (!guess) {
-    document.querySelector('.message').textContent = '⛔ No number';
+    displayMessage('⛔ No number');
 
     //when you guessed right
   } else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = '🎉 You guessed right';
+    displayMessage('🎉 You guessed right');
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
     document.querySelector('.number').textContent = secretNumber;
@@ -26,24 +31,20 @@ document.querySelector('.check').addEventListener('click', function () {
     }
 
     //when the score is above 1
-  } else if (score > 1) {
-    //when you guessed too low
-    if (guess < secretNumber) {
-      document.querySelector('.message').textContent = '⏬ Too Low!';
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      // document.querySelector('.message').textContent =
+      //   guess < secretNumber ? '⏬ Too Low!' : '⏫ Too High!';
+
+      displayMessage(guess < secretNumber ? '⏬ Too Low!' : '⏫ Too High!');
       score--;
       document.querySelector('.score').textContent = score;
 
-      //when you guessed high
-    } else if (guess > secretNumber) {
-      document.querySelector('.message').textContent = '⏫ Too High!';
-      score--;
-      document.querySelector('.score').textContent = score;
+      //when you hit score 0 withput guessing right
+    } else {
+      displayMessage('😇 Game Over Try again');
+      document.querySelector('.score').textContent = 0;
     }
-
-    //when you hit score 0 withput guessing right
-  } else {
-    document.querySelector('.message').textContent = '😇 Game Over Try again';
-    document.querySelector('.score').textContent = 0;
   }
 });
 
@@ -54,7 +55,7 @@ document.querySelector('.again').addEventListener('click', function () {
   document.querySelector('.score').textContent = score;
   document.querySelector('.guess').value = '';
 
-  document.querySelector('.message').textContent = 'Start guessing...';
+  displayMessage('Start guessing...');
   document.querySelector('.number').textContent = '?';
   document.querySelector('body').style.backgroundColor = '#222';
   document.querySelector('.number').style.width = '15rem';
